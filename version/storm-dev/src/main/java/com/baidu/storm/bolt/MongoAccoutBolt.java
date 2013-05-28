5aabba3c1c8ab52f391e27599c0339269fe1a305
lixiao <lixiao19851026@gmail.com>
2013-05-28 15:13:02 +0800

    add common support for idc pv/ar_time

---
+++ version/storm-dev/src/main/java/com/baidu/storm/bolt/MongoAccoutBolt.java
@@ -0,0 +1,184 @@
+/*
+ * authored by lixiao01@baidu.com
+ * feature account by IDC every 30s to emit in one Mongo record
+ * Date:20130523
+ * */
+
+package com.baidu.storm.bolt;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.bson.BSONObject;
+import backtype.storm.Config;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.topology.base.BaseBasicBolt;
+import com.baidu.storm.common.BaseCounter;
+import com.baidu.storm.common.TupleHelpers;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject; 
+
+import org.json.simple.JSONObject;
+
+
+public class MongoAccoutBolt<T> extends BaseBasicBolt {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public MongoAccoutBolt(String lostYear, long emitFrequencyInSeconds, String logKey, String logValue) {
+		this.lostYear = lostYear;
+		this.emitFrequencyInSeconds = emitFrequencyInSeconds;
+		this.logKey = logKey;
+		this.logValue = logValue;
+	}
+	
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		// TODO Auto-generated method stub
+		//declarer.declare(new Fields("hostname","lognumber", "sumall", "averagesum"));
+		declarer.declare(new Fields("records"));
+	}
+
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		// TODO Auto-generated method stub
+		Map<String, Object> conf = new HashMap<String, Object>();
+		conf.put(Config.TOPOLOGY_TICK_TUPLE_FREQ_SECS, emitFrequencyInSeconds);
+		return conf;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void prepare(Map stormConf, TopologyContext context) {
+		// TODO Auto-generated method stub
+		objCounter = new BaseCounter<Object>();
+	}
+
+	@Override
+	public void execute(Tuple input, BasicOutputCollector collector) {
+		this._collector = collector;
+		if (TupleHelpers.isTickTuple(input)) {
+			emitCurrentCounts();
+		} else {
+			countObjAndAck(input);
+		}
+	}
+
+	private void countObjAndAck(Tuple input) {
+		// TODO Auto-generated method stub
+		DBObject object = getDBObjectForInput(input);
+		if(object.get("Document") != null) {
+			String recordValue = null;
+			Long counterValue = null;
+			String recordKey = null;
+			try {
+				recordValue = ((BSONObject)object.get("Document")).get(this.logValue).toString();
+				counterValue = Long.valueOf(recordValue);
+				recordKey = ((BSONObject)object.get("Document")).get(this.logKey).toString();
+			} catch (NullPointerException e) {
+				recordValue = null;
+				counterValue = null;
+				recordKey = null;
+				e.printStackTrace();
+			}
+			objCounter.incrementCount(recordKey, counterValue);
+			//this._collector.ack(input);
+		}
+	}
+
+	private void emitCurrentCounts() {
+		// TODO Auto-generated method stub
+		try {
+			Map<Object, Long> counts = objCounter.getCounts();
+			emit(counts);
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+		//objCounter.wipeArrayList();
+		objCounter.wipeObjects();
+		//innerRecords.clear();
+		records.clear();
+	}
+	
+	@SuppressWarnings("all")
+	private void emit(Map<Object, Long> counts) {
+		// TODO Auto-generated method stub
+		//ensure to clean
+		//innerRecords.clear();
+		//records.clear();
+		records = new JSONObject();
+		
+		for(Entry<Object, Long> entry : counts.entrySet()) {
+			Object obj = entry.getKey();
+			Long countValue = entry.getValue();
+			long sumValue = countValue.longValue();
+			int arraySize = objCounter.computeObjectSize(obj);
+			double avgResponseTime = sumValue / arraySize;
+			Integer pvObj = Integer.valueOf(arraySize);
+			Double arTimeObj = Double.valueOf(avgResponseTime);
+		    //this._collector.emit(new Values(obj, size, count, avgRT));
+			/*records should like this 
+			 *{JX:{PV:123,AR_TIME:456},TC:{PV:123,AR_TIME:456}...,ST:{PV:XXX,AR_TIME:XXX}}
+			*/
+			System.out.println("$$$$$$$$$$$$$$@@IDC:" + obj.toString() +
+					"%%%%%%%%%%%%%%%%%%@@ pvObj" + pvObj.toString() +
+					"&&&&&&&&&&&&&&&&&&@@ arTimeObj"+ arTimeObj.toString());
+			synchronized(this) {
+				//innerRecords.clear();
+				innerRecords = new JSONObject();
+			}
+			innerRecords.put("PV", pvObj);
+			innerRecords.put("AR_TIME", arTimeObj);
+			records.put(obj.toString(), innerRecords);
+		}
+		this._collector.emit(new Values(records.toString()));
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+	}
+	
+	public DBObject getDBObjectForInput(Tuple input) {
+		BasicDBObjectBuilder dbObjectBuilder = new BasicDBObjectBuilder();
+		for (String field : input.getFields()) {
+			 Object value = input.getValueByField(field);
+			 if (isValidDBObjectField(value)) {
+				 dbObjectBuilder.append(field, value);
+			 }
+		}
+		return dbObjectBuilder.get();
+	}
+	
+	private boolean isValidDBObjectField(Object value) {
+		return value instanceof String
+				|| value instanceof Date
+				|| value instanceof Integer
+				|| value instanceof Float
+				|| value instanceof Double
+				|| value instanceof Short
+				|| value instanceof Long
+				|| value instanceof DBObject;
+	}
+	
+	private String lostYear;
+	private String logKey;
+	private String logValue;
+	private long emitFrequencyInSeconds;
+	private BaseCounter<Object> objCounter;
+	private BasicOutputCollector _collector;
+	private JSONObject records;
+	private JSONObject innerRecords;
+
+}
---
+++ version/storm-dev/src/main/java/com/baidu/storm/spout/MongoSpout.java
@@ -0,0 +1,183 @@
+package com.baidu.storm.spout;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.json.simple.parser.JSONParser;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichSpout;
+import backtype.storm.utils.Utils;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.Bytes;
+import com.mongodb.DB;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+/**
+* A Spout which consumes documents from a Mongodb tailable cursor.
+*
+* Subclasses should simply override two methods:
+* <ul>
+* <li>{@link #declareOutputFields(OutputFieldsDeclarer) declareOutputFields}
+* <li>{@link #dbObjectToStormTuple(DBObject) dbObjectToStormTuple}, which turns
+* a Mongo document into a Storm tuple matching the declared output fields.
+* </ul>
+*
+** <p>
+* <b>WARNING:</b> You can only use tailable cursors on capped collections.
+* 
+* @author Dan Beaulieu <danjacob.beaulieu@gmail.com>
+* @modifier Xiao Li <lixiao19851026@gmail.com> 2013-04-27
+*
+*/
+public abstract class MongoSpout implements IRichSpout {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private SpoutOutputCollector collector;
+	
+	private LinkedBlockingQueue<DBObject> queue;
+	private final AtomicBoolean opened = new AtomicBoolean(false);
+	
+	private DB mongoDB;
+	private final DBObject query;
+	
+	private final String mongoHost;
+	private final int mongoPort;
+	private final String mongoDbName;
+	private final String mongoCollectionName;
+	static JSONParser jsonParser = new JSONParser();
+	
+	
+	public MongoSpout(String mongoHost, int mongoPort, String mongoDbName, 
+			String mongoCollectionName, DBObject query) {
+		
+		this.mongoHost = mongoHost;
+		this.mongoPort = mongoPort;
+		this.mongoDbName = mongoDbName;
+		this.mongoCollectionName = mongoCollectionName;
+		this.query = query;
+	}
+	
+	class TailableCursorThread extends Thread {
+		
+		LinkedBlockingQueue<DBObject> queue;
+		String mongoCollectionName;
+		DB mongoDB;
+		DBObject query;
+
+		public TailableCursorThread(LinkedBlockingQueue<DBObject> queue, DB mongoDB, 
+				String mongoCollectionName, DBObject query) {
+			
+			this.queue = queue;
+			this.mongoDB = mongoDB;
+			this.mongoCollectionName = mongoCollectionName;
+			this.query = query;
+		}
+
+		public void run() {
+			
+			while(opened.get()) {
+				try {
+					// create the cursor
+					mongoDB.requestStart();
+					final DBCursor cursor = mongoDB.getCollection(mongoCollectionName)
+												//tail -n 1
+												.find(query)
+												.sort(new BasicDBObject("_id", -1)).limit(5000)
+												//head -n 1
+												//.sort(new BasicDBObject("$natural", 1)).limit(1)
+												//.addOption(Bytes.QUERYOPTION_TAILABLE)
+												.addOption(Bytes.QUERYOPTION_AWAITDATA)
+												.addOption(Bytes.QUERYOPTION_NOTIMEOUT);
+					try {
+						while (opened.get() && cursor.hasNext()) {
+		                    final DBObject doc = cursor.next();
+		
+		                    if (doc == null) break;
+		
+		                    queue.put(doc);
+		                }
+					} finally {
+						try { 
+							if (cursor != null) cursor.close(); 
+						} catch (final Throwable t) { }
+	                    try { 
+	                    	mongoDB.requestDone(); 
+	                    	} catch (final Throwable t) { }
+	                }
+					
+					Utils.sleep(10);
+				} catch (final MongoException.CursorNotFound cnf) {
+					// rethrow only if something went wrong while we expect the cursor to be open.
+                    if (opened.get()) {
+                    	throw cnf;
+                    }
+                } catch (InterruptedException e) { break; }
+			}
+		};
+	}
+	
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		
+		this.collector = collector;
+		this.queue = new LinkedBlockingQueue<DBObject>(50000);
+		try {
+			this.mongoDB = new Mongo(this.mongoHost, this.mongoPort).getDB(this.mongoDbName);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		TailableCursorThread listener = new TailableCursorThread(this.queue, this.mongoDB, 
+				this.mongoCollectionName, this.query);
+		this.opened.set(true);
+		listener.start();
+	}
+
+	@Override
+	public void close() {
+		this.opened.set(false);
+	}
+
+	@Override
+	public void nextTuple() {
+		
+		DBObject dbo = this.queue.poll();
+	/*	try {
+			json = jsonParser.parse(dbo.toString());
+			//System.out.println(json);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		if(dbo == null) {
+            Utils.sleep(50);
+        } else {
+            this.collector.emit(dbObjectToStormTuple(dbo));
+        	//this.collector.emit(new Values(json));
+        }
+	}
+
+	@Override
+	public void ack(Object msgId) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public void fail(Object msgId) {
+		// TODO Auto-generated method stub	
+	}
+	
+	public abstract List<Object> dbObjectToStormTuple(DBObject message);
+
+}
